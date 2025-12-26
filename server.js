2a6cebc3d456b57c50e554a98fcc4b75ce8ebe9d
Muhammadsaid Nuriddinov <charlie20040915@gmail.com>
2025-12-26 21:22:43 +0900

    BBR: PORTFOLIO

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const  http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 
 //1: KRISH code
@@ -25,13 +35,17 @@ app.post('/create-item', (req, res) => {
     res.json({test: 'success'});
 });
 
+app.get("/author", (req, res) => {
+    res.render("author.ejs", { user : user  });
+});
+
 app.get('/', function(req, res) {
-    res.render("harid");
+    res.render("harid.ejs");
 });
 
 
 const server  = http.createServer(app);
-let PORT = 3000;
+let PORT = 4000;
 server.listen(PORT, function() {
     console.log(`The server successfully on port: ${PORT}`);
 });
